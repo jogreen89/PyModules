2aa936a520d122688e99d0a6182cece0c251ecb6
zubernetes <zubernetes@gmail.com>
2016-10-13 09:54:33 -0400

    Making some small progress, put strangely not getting back inserted data

--- tiny_db.py
+++ tiny_db.py
@@ -2,8 +2,10 @@
 #!/usr/bin/python
 
 # Imports
-
-#from tinydb import TinyDB
-
+from tinydb import TinyDB
 # Print a message to console
-print("Hello World.")
+print("Hello TinyDB.")
+db = TinyDB('db.json')
+db.insert({'type': 'apple', 'count': 7})
+db.insert({'type': 'peach', 'count': 3})
+db.all()
